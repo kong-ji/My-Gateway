86e14b07171caee17ac483d42626abf9254f4cd4
kongji <2718084748@qq.com>
2025-09-05 17:44:30 +0800

    feat: config模块新增服务定义与实例

---
+++ My-Gateway-Config/src/main/java/pojo/ServiceInstance.java
@@ -0,0 +1,57 @@
+package pojo;
+
+import lombok.Data;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 具体服务实例
+ */
+@Data
+public class ServiceInstance implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -7137947815268291319L;
+
+    /**
+     * 服务名
+     */
+    private String serviceName;
+
+    /**
+     * 实例id：ip:port
+     */
+    private String instanceId;
+
+    /**
+     * 服务实例 ip
+     */
+    private String ip;
+
+    /**
+     * 服务实例 port
+     */
+    private int port;
+
+    /**
+     * 权重信息
+     */
+    private int weight = 1;
+
+    /**
+     * 服务实例是否启用
+     */
+    private boolean enable = true;
+
+    /**
+     * 服务实例是否灰度
+     */
+    private boolean gray;
+
+    /**
+     * 服务实例灰度比例
+     */
+    private double threshold;
+
+}
