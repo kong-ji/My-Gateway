5d61df2ec1794948ff1462f3920a6eec545af8e1
kongji <2718084748@qq.com>
2025-09-08 18:50:47 +0800

    feat: 完成响应结果构建

---
+++ My-Gateway-Core/src/test/java/TestJson.java
@@ -0,0 +1,23 @@
+
+import cn.hutool.json.JSONUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import config.Config;
+import org.junit.Test;
+
+public class TestJson {
+
+    @Test
+    public void testJson() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode objectNode = mapper.createObjectNode();
+        Config config = new Config();
+        objectNode.putPOJO("data", config);
+        System.out.println(mapper.writeValueAsString(config));
+
+        System.out.println(JSONUtil.toJsonStr(config));
+    }
+
+}
